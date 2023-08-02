538075aa574531cb851d8cff91ed3489bee47e36
Christopher Imoni <cimoni@Chris-MacBook-Pro.local>
2023-08-02 19:44:41 +0100

    initial commit

---
+++ src/test/java/com/chrisimoni/springbatchprj/SpringBatchPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisimoni.springbatchprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
